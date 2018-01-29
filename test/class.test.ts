baed0d29ef96537c2463d6dbcffaf71afdb4b3a2
Sanghyun Lee <hammerha@gmail.com>
2018-01-29 15:05:42 +0100

    Add tests of Class

---
+++ test/class.test.ts
@@ -0,0 +1,33 @@
+describe("Class", () => {
+  it("Basic", () => {
+    class Greeter {
+      greeting: string;
+      constructor(message: string) {
+        this.greeting = message;
+      }
+      greet() {
+        return "Hello, " + this.greeting;
+      }
+    }
+
+    expect(new Greeter("world").greet()).toBe("Hello, world");
+  });
+
+  it("Accessors", () => {
+    class Person {
+      private _name;
+
+      set name(name: string) {
+        this._name = name;
+      }
+
+      get name() {
+        return this._name;
+      }
+    }
+
+    const p: Person = new Person();
+    p.name = "abc";
+    expect(p.name).toBe("abc");
+  });
+});
