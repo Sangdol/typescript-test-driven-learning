7114b86ee53105a2919a76ee8a66d16b66af7f1c
Sanghyun Lee <hammerha@gmail.com>
2023-03-05 17:39:06 +0100

    Add more sinon tests

--- test/sinon.spec.ts
+++ test/sinon.spec.ts
@@ -7,19 +7,69 @@ function assert(condition: boolean) {
   }
 }
 
+/**
+ * https://sinonjs.org/releases/latest/spies/
+ */
 describe("sinon spy", () => {
   it("should spy work", () => {
     const spy = sinon.spy();
     spy();
     assert(spy.called);
   });
+
+  it("should spy work with args", () => {
+    const spy = sinon.spy();
+    spy(42);
+    assert(spy.calledWith(42));
+  });
+
+  it("should spy work with obj", () => {
+    const obj = {
+      foo() {
+        return 42;
+      },
+    };
+    const spy = sinon.spy(obj, "foo");
+    assert(obj.foo() === 42);
+    assert(spy.calledOnce);
+
+    assert(obj.foo() === 42);
+    assert(spy.calledTwice);
+
+    spy.restore();
+
+    assert(obj.foo() === 42);
+    assert(spy.calledTwice);
+  });
 });
 
+/**
+ * https://sinonjs.org/releases/latest/stubs/
+ */
 describe("sinon stub", () => {
   it("should stub work", () => {
     const stub = sinon.stub();
     stub.returns(42);
     assert(stub() === 42);
+  });
+
+  it("should stub work with obj", () => {
+    const obj = {
+      foo() {
+        return 42;
+      },
+    };
+    const stub = sinon.stub(obj, "foo").returns(42);
+    assert(obj.foo() === 42);
+    assert(stub.calledOnce);
+
+    assert(obj.foo() === 42);
+    assert(stub.calledTwice);
+
+    stub.restore();
+
+    assert(obj.foo() === 42);
+    assert(stub.calledTwice);
   });
 });
 
@@ -30,6 +80,26 @@ describe("sinon fake", () => {
   it("should fake work", () => {
     const fake = sinon.fake.returns(42);
     assert(fake() === 42);
+  });
+
+  it("should fake work with obj", () => {
+    const obj = {
+      foo() {
+        return 42;
+      },
+    };
+    const fake = sinon.fake.returns(42);
+    sinon.replace(obj, "foo", fake);
+    assert(obj.foo() === 42);
+    assert(fake.calledOnce);
+
+    assert(obj.foo() === 42);
+    assert(fake.calledTwice);
+
+    sinon.restore();
+
+    assert(obj.foo() === 42);
+    assert(fake.calledTwice);
   });
 });
 
