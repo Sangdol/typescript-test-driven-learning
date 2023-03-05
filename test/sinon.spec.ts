b2fec54867a58e8abed6ad283172872bdc6516fe
Sanghyun Lee <hammerha@gmail.com>
2023-03-05 17:30:17 +0100

    Add basic sinon tests

--- test/sinon.spec.ts
+++ test/sinon.spec.ts
@@ -1,10 +1,53 @@
 import "mocha";
 import sinon from "sinon";
 
-describe("sinon", () => {
-  it("should work", () => {
+function assert(condition: boolean) {
+  if (!condition) {
+    throw new Error("Assertion failed");
+  }
+}
+
+describe("sinon spy", () => {
+  it("should spy work", () => {
     const spy = sinon.spy();
     spy();
-    spy.should.have.been.calledOnce;
+    assert(spy.called);
+  });
+});
+
+describe("sinon stub", () => {
+  it("should stub work", () => {
+    const stub = sinon.stub();
+    stub.returns(42);
+    assert(stub() === 42);
+  });
+});
+
+/**
+ * https://sinonjs.org/releases/latest/fakes/
+ */
+describe("sinon fake", () => {
+  it("should fake work", () => {
+    const fake = sinon.fake.returns(42);
+    assert(fake() === 42);
+  });
+});
+
+/**
+ * https://sinonjs.org/releases/latest/mocks/
+ */
+describe("sinon mock", () => {
+  it("should mock work", () => {
+    const myObj = {
+      foo() {
+        return 42;
+      },
+    };
+
+    const mock = sinon.mock(myObj);
+    mock.expects("foo").once().returns(42);
+
+    assert(myObj.foo() === 42);
+    mock.verify();
   });
 });
