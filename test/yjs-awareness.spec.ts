5466e40bb77fe527ec27329df612a155e5e27124
Sanghyun Lee <hammerha@gmail.com>
2023-03-19 21:28:58 +0100

    Add more yjs awareness tests

--- test/yjs-awareness.spec.ts
+++ test/yjs-awareness.spec.ts
@@ -11,7 +11,7 @@ import * as awareness from "y-protocols/awareness";
  * https://github.com/yjs/y-protocols/blob/master/awareness.test.js
  */
 describe("yjs awareness", function (this: Mocha.Suite) {
-  it.skip("should udpate events work", () => {
+  it("should udpate events work", () => {
     const ydoc1 = new YDoc();
     ydoc1.clientID = 1;
     const ydoc2 = new YDoc();
@@ -21,7 +21,6 @@ describe("yjs awareness", function (this: Mocha.Suite) {
     const aw2 = new awareness.Awareness(ydoc2);
 
     // Doc: "Listens to awareness changes"
-    // Why does it listen to state changes?
     aw1.on("update", ({ added, updated, removed }) => {
       const enc = awareness.encodeAwarenessUpdate(
         aw1,
@@ -80,5 +79,116 @@ describe("yjs awareness", function (this: Mocha.Suite) {
 
     expect(lastChangeLocal).to.deep.equal({ added: [], updated: [1], removed: [] });
     expect(lastChange).to.deep.equal({ added: [1], updated: [], removed: [] });
+  });
+
+  it("should events trigger in order", () => {
+    const ydoc1 = new YDoc();
+    ydoc1.clientID = 1;
+
+    const ydoc2 = new YDoc();
+    ydoc2.clientID = 2;
+
+    const aw1 = new awareness.Awareness(ydoc1);
+    const aw2 = new awareness.Awareness(ydoc2);
+
+    let order = 0;
+
+    aw1.on("change", (change: {added: [], updated: [], removed: []}) => {
+      console.log("aw1 change", change);
+      expect(order).to.equal(0);
+      order++;
+    });
+
+    aw1.on("update", ({ added, updated, removed }) => {
+      const enc = awareness.encodeAwarenessUpdate(
+        aw1,
+        added.concat(updated).concat(removed)
+      );
+
+      // Before applying the update
+      expect(order).to.equal(1);
+      order++;
+
+      awareness.applyAwarenessUpdate(aw2, enc, "custom");
+
+      // After applying the update
+      expect(order).to.equal(4);
+      order++;
+    });
+
+    aw2.on("change", (change: {added: [], updated: [], removed: []}) => {
+      console.log("aw2 change", change);
+      expect(order).to.equal(2);
+      order++;
+    });
+
+    aw2.on("update", ({ added, updated, removed }) => {
+      console.log("aw2 update");
+      expect(order).to.equal(3);
+      order++;
+    });
+
+    aw1.setLocalState({ x: 3 });
+  });
+
+  it("should only update be triggered on no-change event", () => {
+    const ydoc1 = new YDoc();
+    ydoc1.clientID = 1;
+
+    const ydoc2 = new YDoc();
+    ydoc2.clientID = 2;
+
+    const aw1 = new awareness.Awareness(ydoc1);
+    const aw2 = new awareness.Awareness(ydoc2);
+
+    let aw1ChangeCount = 0;
+    let aw2ChangeCount = 0;
+    let aw1UpdateCount = 0;
+    let aw2UpdateCount = 0;
+
+    // 'change' doesn't trigger when there is no change
+    aw1.on("change", (change: {added: [], updated: [], removed: []}) => {
+      aw1ChangeCount++;
+    });
+
+    // 'update' triggers even when there is no change
+    aw1.on("update", ({ added, updated, removed }) => {
+      const enc = awareness.encodeAwarenessUpdate(
+        aw1,
+        added.concat(updated).concat(removed)
+      );
+
+      awareness.applyAwarenessUpdate(aw2, enc, "custom");
+      aw1UpdateCount++;
+    });
+
+    aw2.on("change", (change: {added: [], updated: [], removed: []}) => {
+      aw2ChangeCount++;
+    });
+
+    aw2.on("update", ({ added, updated, removed }) => {
+      aw2UpdateCount++;
+    });
+
+    aw1.setLocalState({ x: 3 });
+
+    expect(aw1ChangeCount).to.equal(1);
+    expect(aw2ChangeCount).to.equal(1);
+    expect(aw1UpdateCount).to.equal(1);
+    expect(aw2UpdateCount).to.equal(1);
+
+    aw1.setLocalState({ x: 3 });
+
+    expect(aw1ChangeCount).to.equal(1);
+    expect(aw2ChangeCount).to.equal(1);
+    expect(aw1UpdateCount).to.equal(2);
+    expect(aw2UpdateCount).to.equal(2);
+
+    aw1.setLocalState({ x: 4 });
+
+    expect(aw1ChangeCount).to.equal(2);
+    expect(aw2ChangeCount).to.equal(2);
+    expect(aw1UpdateCount).to.equal(3);
+    expect(aw2UpdateCount).to.equal(3);
   });
 });
