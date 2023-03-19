4e53475688e090866944be637416963521c7f430
Sanghyun Lee <hammerha@gmail.com>
2023-03-19 20:56:03 +0100

    Add yjs awareness change event tests

--- test/yjs-awareness.spec.ts
+++ test/yjs-awareness.spec.ts
@@ -11,10 +11,9 @@ import * as awareness from "y-protocols/awareness";
  * https://github.com/yjs/y-protocols/blob/master/awareness.test.js
  */
 describe("yjs awareness", function (this: Mocha.Suite) {
-  it("should sync awareness", async () => {
+  it.skip("should udpate events work", () => {
     const ydoc1 = new YDoc();
     ydoc1.clientID = 1;
-
     const ydoc2 = new YDoc();
     ydoc2.clientID = 2;
 
@@ -34,9 +33,6 @@ describe("yjs awareness", function (this: Mocha.Suite) {
     expect(aw1.meta?.get(1)?.clock).to.equal(0);
     expect(aw2.meta?.get(1)?.clock).to.equal(undefined);
 
-    console.log("aw1 meta", aw1.meta);
-    console.log("aw2 meta", aw2.meta);
-
     aw1.setLocalState({ x: 3 });
 
     // Why from 1 unlike the original test code?
@@ -47,5 +43,42 @@ describe("yjs awareness", function (this: Mocha.Suite) {
     expect(aw2.meta?.get(2)?.clock).to.equal(0);
     expect(aw2.meta?.get(1)?.clock).to.equal(1);
     expect(aw1.meta?.get(1)?.clock).to.equal(1);
+  });
+
+  it("should change events work", () => {
+    const ydoc1 = new YDoc();
+    ydoc1.clientID = 1;
+
+    const ydoc2 = new YDoc();
+    ydoc2.clientID = 2;
+
+    const aw1 = new awareness.Awareness(ydoc1);
+    const aw2 = new awareness.Awareness(ydoc2);
+
+    aw1.on("update", ({ added, updated, removed }) => {
+      const enc = awareness.encodeAwarenessUpdate(
+        aw1,
+        added.concat(updated).concat(removed)
+      );
+      awareness.applyAwarenessUpdate(aw2, enc, "custom");
+    });
+
+    let lastChangeLocal = {};
+    // Listens to state changes
+    aw1.on("change", (change: {added: [], updated: [], removed: []}) => {
+      console.log("aw1 change", change);
+      lastChangeLocal = change;
+    });
+
+    let lastChange = {};
+    aw2.on("change", (change: {added: [], updated: [], removed: []}) => {
+      console.log("aw2 change", change);
+      lastChange = change;
+    });
+
+    aw1.setLocalState({ x: 3 });
+
+    expect(lastChangeLocal).to.deep.equal({ added: [], updated: [1], removed: [] });
+    expect(lastChange).to.deep.equal({ added: [1], updated: [], removed: [] });
   });
 });
