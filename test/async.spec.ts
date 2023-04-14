319d95309e8ae50eb1d4f52fe7ae772bd77326b8
Sanghyun Lee <hammerha@gmail.com>
2023-04-14 13:19:00 +0200

    Format

--- test/async.spec.ts
+++ test/async.spec.ts
@@ -1,30 +1,37 @@
 import "mocha";
 import { expect } from "chai";
 
-describe("Async", function(this: Mocha.Suite) {
+describe("Async", function (this: Mocha.Suite) {
   it("Promise.allSettled", async () => {
     const error = async (): Promise<number> => {
       throw new Error("Hallo");
-    }
+    };
     const nonError = async (): Promise<number> => {
       return 1;
-    }
+    };
 
     const results = await Promise.allSettled([error(), nonError()]);
     expect(results.length).to.be.equal(2);
 
     expect(results[0].status).to.be.equal("rejected");
-    expect((results[0] as PromiseRejectedResult).reason.message).to.be.equal("Hallo");
+    expect((results[0] as PromiseRejectedResult).reason.message).to.be.equal(
+      "Hallo"
+    );
 
     expect(results[1].status).to.be.equal("fulfilled");
     expect((results[1] as PromiseFulfilledResult<number>).value).to.be.equal(1);
 
     // Using filter and type guards
-    const fulfilled = results.filter((result): result is PromiseFulfilledResult<number> => result.status === "fulfilled");
+    const fulfilled = results.filter(
+      (result): result is PromiseFulfilledResult<number> =>
+        result.status === "fulfilled"
+    );
     expect(fulfilled.length).to.be.equal(1);
     expect(fulfilled[0].value).to.be.equal(1);
 
-    const rejected = results.filter((result): result is PromiseRejectedResult => result.status === "rejected");
+    const rejected = results.filter(
+      (result): result is PromiseRejectedResult => result.status === "rejected"
+    );
     expect(rejected.length).to.be.equal(1);
     expect(rejected[0].reason.message).to.be.equal("Hallo");
   });
