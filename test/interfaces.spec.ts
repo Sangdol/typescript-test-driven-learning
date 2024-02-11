88af2fb1fbb2ebc5e98a15f9c49961865abb5e2f
Sanghyun Lee <hammerha@gmail.com>
2024-02-11 16:17:28 +0100

    Add index signature and intersection types tests

--- test/interfaces.spec.ts
+++ test/interfaces.spec.ts
@@ -13,6 +13,62 @@ describe("interfaces", function (this: Mocha.Suite) {
 
     // No other properties are allowed from TypeScript 1.6
     // https://stackoverflow.com/questions/31816061/why-am-i-getting-an-error-object-literal-may-only-specify-known-properties
-    expect(returnLabel({label: "hi"})).to.equal("hi");
+    expect(returnLabel({ label: "hi" })).to.equal("hi");
+  });
+
+  it("Index signature", () => {
+    interface StringArray {
+      [index: number]: string;
+    }
+
+    const arr: StringArray = ["a", "b"];
+    expect(arr[0]).to.equal("a");
+  });
+
+  it("Excess Property Checks", () => {
+    // https://www.typescriptlang.org/docs/handbook/2/objects.html#excess-property-checks
+    interface Person {
+      name: string;
+      age: number;
+    }
+
+    const p: Person = {
+      name: "name",
+      age: 10,
+      // @ts-expect-error Error: Object literal may only specify known properties
+      occupation: "occupation",
+      // Error: Type '{ name: string; age: number; occupation: string; }'
+      // has no properties in common with type 'Person'.
+    };
+    expect(p.name).to.equal("name");
+
+    // Solution: type assertion
+    const p2: Person = {
+      name: "name",
+      age: 10,
+      occupation: "occupation",
+    } as Person;
+
+    expect(p2.name).to.equal("name");
+  });
+
+  it("Intersection Types", () => {
+    interface Colorful {
+      color: string;
+    }
+
+    interface Circle {
+      radius: number;
+    }
+
+    type ColorfulCircle = Colorful & Circle;
+
+    const draw = (circle: ColorfulCircle) => {
+      return `Drawing a ${circle.color} circle with radius ${circle.radius}`;
+    };
+
+    expect(draw({ color: "red", radius: 10 })).to.equal(
+      "Drawing a red circle with radius 10"
+    );
   });
 });
