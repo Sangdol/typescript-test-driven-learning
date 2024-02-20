372cda490c3715fbd2eb2c265154e4d4d02a21ab
Sanghyun Lee <hammerha@gmail.com>
2024-02-20 07:44:15 +0100

    Add generator tests

--- test/syntax.spec.ts
+++ test/syntax.spec.ts
@@ -14,5 +14,30 @@ describe("Syntax", function (this: Mocha.Suite) {
     expect(false || 1).to.be.equal(1);
     expect(false ?? 1).to.be.equal(false);
   });
-});
 
+  it("generator", () => {
+    function* gen() {
+      yield 1;
+      yield 2;
+      yield 3;
+    }
+
+    const g = gen();
+    expect(g.next().value).to.be.equal(1);
+    expect(g.next().value).to.be.equal(2);
+    expect(g.next().value).to.be.equal(3);
+    expect(g.next().done).to.be.true;
+
+    function* gen2() {
+      yield* [1, 2, 3];
+      return 4;
+    }
+
+    const g2 = gen2();
+    expect(g2.next().value).to.be.equal(1);
+    expect(g2.next().value).to.be.equal(2);
+    expect(g2.next().value).to.be.equal(3);
+    expect(g2.next().value).to.be.equal(4);
+    expect(g2.next().done).to.be.true;
+  });
+});
