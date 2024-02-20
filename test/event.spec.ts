537dc6935e26ea5b7d0ff19a52a18d8a5cd595fe
Sanghyun Lee <hammerha@gmail.com>
2024-02-20 09:40:38 +0100

    Add events tests

---
+++ test/event.spec.ts
@@ -0,0 +1,60 @@
+import "mocha";
+import { expect } from "chai";
+
+describe("Event", function (this: Mocha.Suite) {
+  it("should work", () => {
+    const event = new Event("click");
+    expect(event.type).to.equal("click");
+  });
+
+  it("should work with options", () => {
+    const event = new Event("click", {
+      bubbles: true,
+      cancelable: true,
+    });
+    expect(event.bubbles).to.be.true;
+    expect(event.cancelable).to.be.true;
+  });
+
+  it("should work with custom event", () => {
+    class MyEvent extends Event {
+      constructor() {
+        super("click");
+      }
+    }
+
+    const event = new MyEvent();
+    expect(event.type).to.equal("click");
+  });
+
+  it("should work with custom event and options", () => {
+    class MyEvent extends Event {
+      constructor() {
+        super("click", {
+          bubbles: true,
+          cancelable: true,
+        });
+      }
+    }
+
+    const event = new MyEvent();
+    expect(event.bubbles).to.be.true;
+    expect(event.cancelable).to.be.true;
+  });
+});
+
+describe("MessageEvent", function (this: Mocha.Suite) {
+  it("should work", () => {
+    const event = new MessageEvent("message");
+    expect(event.type).to.equal("message");
+  });
+
+  it("should work with options", () => {
+    const event = new MessageEvent("message", {
+      data: "Hello",
+      origin: "http://localhost",
+    });
+    expect(event.data).to.equal("Hello");
+    expect(event.origin).to.equal("http://localhost");
+  });
+});
