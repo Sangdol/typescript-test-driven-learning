543700f5498645f1f71db7602b2681b62482d159
Sanghyun Lee <hammerha@gmail.com>
2024-03-03 16:42:48 +0100

    Add function prototypes tests

---
+++ test/prototypes.spec.ts
@@ -0,0 +1,61 @@
+import "mocha";
+import { expect } from "chai";
+
+/* eslint-disable */
+
+describe("Prototypes", () => {
+  /**
+   * https://miro.com/app/board/uXjVNdWbpgU=/?moveToWidget=3458764579007190494&cot=14
+   */
+  it("function prototype chain", () => {
+    function Pet(name: string) {
+      // @ts-ignore
+      this.name = name;
+    }
+
+    Pet.prototype.getName = function () {
+      return this.name;
+    };
+
+    function Dog(name: string, breed: string) {
+      // @ts-ignore
+      Pet.call(this, name);
+      // @ts-ignore
+      this.breed = breed;
+    }
+
+    // Same as Dog.prototype.__proto__ = Pet.prototype;
+    Dog.prototype = Object.create(Pet.prototype);
+
+    Dog.prototype.getBreed = function () {
+      return this.breed;
+    };
+
+    const dog = new Dog("Fido", "Golden Retriever");
+
+    //
+    // prototype chain
+    //
+    expect(dog.getName()).to.equal("Fido");
+    expect(dog.__proto__).to.equal(Dog.prototype);
+    expect(dog.prototype).to.equal(undefined);
+
+    expect(dog.__proto__.__proto__).to.equal(Pet.prototype);
+    expect(Dog.prototype.__proto__).to.equal(Pet.prototype);
+
+    expect(dog.__proto__.__proto__.__proto__).to.equal(Object.prototype);
+    expect(Pet.prototype.__proto__).to.equal(Object.prototype);
+
+    expect(dog.__proto__.__proto__.__proto__.__proto__).to.equal(null);
+    // @ts-ignore
+    expect(Object.prototype.__proto__).to.equal(null);
+
+    //
+    // Function: every function is a Function object
+    //
+    // @ts-ignore
+    expect(Dog.__proto__).to.equal(Function.prototype);
+    // @ts-ignore
+    expect(Function.prototype.__proto__).to.equal(Object.prototype);
+  });
+});
